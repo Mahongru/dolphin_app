55725415c4d0cb115af1582a0e41a06365499439
Hongru Ma <mahongru.dev@gmail.com>
2022-11-12 01:34:47 +0200

    Added ticker class for play function

---
+++ lib/ticker.dart
@@ -0,0 +1,8 @@
+// Borrowed from https://bloclibrary.dev/#/fluttertimertutorial
+class Ticker {
+  const Ticker();
+  Stream<int> tick({required int ticks}) {
+    return Stream.periodic(const Duration(seconds: 2), (x) => ticks - x - 1)
+        .take(ticks);
+  }
+}
