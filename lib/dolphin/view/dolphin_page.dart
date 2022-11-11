6bb4804cec84d1c36bae84fc169e52a487a4278b
Hongru Ma <mahongru.dev@gmail.com>
2022-11-11 14:49:01 +0200

    Added background and actions widget

--- lib/dolphin/view/dolphin_page.dart
+++ lib/dolphin/view/dolphin_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// TODO: Separate into view
 class DolphinPage extends StatelessWidget {
   const DolphinPage({Key? key, required this.title}) : super(key: key);
 
@@ -8,13 +9,51 @@ class DolphinPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [Text('Welcome to the new dolphin app!')],
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Stack(children: [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100),
+                // TODO: Implement dolphin view
+                child: Center(child: Text('Welcome to the new dolphin app')),
+              ),
+            ],
+          ),
+          Actions(),
+        ]));
+  }
+}
+
+class Actions extends StatelessWidget {
+  const Actions({super.key});
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implement dolphin feature actions
+    return Center();
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue.shade50,
+              Colors.blue.shade500,
+            ],
+          ),
         ),
       ),
     );
