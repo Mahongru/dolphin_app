2b117bd73edd0eb0eb0d9d49e1e39b129e0064e7
Hongru Ma <mahongru.dev@gmail.com>
2022-11-12 01:32:17 +0200

    Added MultiRepositoryProvider

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_app/dolphin/dolphin.dart';
 
 class App extends StatelessWidget {
@@ -8,15 +9,20 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dolphin App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: const Color.fromRGBO(109, 234, 255, 1),
-        colorScheme: const ColorScheme.light(
-          secondary: Color.fromRGBO(72, 74, 126, 1),
+        title: 'Dolphin App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: const Color.fromRGBO(109, 234, 255, 1),
+          colorScheme: const ColorScheme.light(
+            secondary: Color.fromRGBO(72, 74, 126, 1),
+          ),
         ),
-      ),
-      home: const DolphinPage(),
-    );
+        home: MultiRepositoryProvider(
+          providers: [
+            // Injecting dependency in the upper context
+            RepositoryProvider(create: (context) => DolphinService())
+          ],
+          child: const DolphinPage(),
+        ));
   }
 }
