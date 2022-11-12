42da19e4e90a266cfc185614521d44301c823c3e
Hongru Ma <mahongru.dev@gmail.com>
2022-11-12 11:37:05 +0200

    Reduced Dolphin model and Handle list of Dolphins

--- lib/dolphin/service/dolphin_service.dart
+++ lib/dolphin/service/dolphin_service.dart
@@ -4,61 +4,26 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DolphinModel extends Equatable {
-  // Equatable is designed to only work with immutable objects
-  // so all member variables must be final
-  final String id;
-  final String regularLink;
-  final Map urls;
+  final String url;
+  const DolphinModel({required this.url});
 
-  // create constant constructor
-  const DolphinModel(
-      {required this.id, required this.regularLink, required this.urls});
-
-  // override method for equatable
   @override
   List<Object> get props => [
-        {id, regularLink, urls}
+        {url}
       ];
 
-  // create method to map values to properties
   factory DolphinModel.fromJson(Map<String, dynamic> json) {
-    return DolphinModel(
-        id: json['id'],
-        regularLink: json['urls']['regular'],
-        urls: json['urls']);
+    return DolphinModel(url: json['urls']['small']);
   }
-}
-
-class Urls {
-  final String raw;
-  final String full;
-  final String regular;
-  final String small;
-  final String thumb;
-
-  Urls(this.raw, this.full, this.regular, this.small, this.thumb);
 }
 
 class DolphinService {
   Uri endpoint = Uri.https('api.unsplash.com', '/photos/random', {
     'query': 'dolphin',
     'client_id': dotenv.env['CLIENT_ID'],
-    // TODO: Make API call work with list of images
-    // 'count': '1',
+    'count': '5',
     'orientation': 'squarish'
   });
-
-  Future<DolphinModel> getDolphinImage() async {
-    Response response = await get(endpoint);
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> result = jsonDecode(response.body);
-
-      return DolphinModel.fromJson(result);
-    } else {
-      throw Exception(response.reasonPhrase);
-    }
-  }
 
   Future<List<DolphinModel>> getDolphinImages() async {
     Response response = await get(endpoint);
